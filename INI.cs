fa8c754df12bdd63e66e96e3ea302824652d56cd
K0D3R <BrandonHajduga@GMail.com>
2024-07-19 14:27:24 -0400

    Add files via upload

---
+++ INI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace INI
+{
+	// Create a New INI file to store or load data
+	public class IniFile
+	{
+		public string path;
+
+		[DllImport("kernel32")]
+		private static extern long WritePrivateProfileString(string section,string key,string val,string filePath);
+		[DllImport("kernel32")]
+		private static extern int GetPrivateProfileString(string section,string key,string def,StringBuilder retVal,int size,string filePath);
+
+		// INIFile Constructor.
+		//File Name
+		//Use Config Folder
+		//Config Folder Name
+		public IniFile(string FileName, bool ConfigFolder = false, string ConfigFolderName = "config")
+		{
+			if (ConfigFolder) { path = Environment.CurrentDirectory + "\\" + ConfigFolderName + "\\" + FileName + ".ini"; }
+			else { path = Environment.CurrentDirectory + "\\" + FileName + ".ini"; }
+		}
+
+		// Write Data to the INI File
+		// Section name
+		// Key Name
+		// Value Name
+		public void IniWriteValue(string Section,string Key,string Value)
+		{
+			WritePrivateProfileString(Section,Key,Value,path);
+		}
+
+		/// Read Data Value From the Ini File
+		// Section name
+		// Key Name
+		// Value Name
+		public string IniReadValue(string Section,string Key)
+		{
+			StringBuilder temp = new StringBuilder(255);
+			int i = GetPrivateProfileString(Section,Key,"",temp,255,path);
+			return temp.ToString();
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using INI;
+using System.Diagnostics;
+
+IniFile ini = new IniFile("Launcher", false, "");
+string ACTPath;
+string LauncherPath;
+
+ACTPath = ini.IniReadValue("Paths", "ACT");
+Console.WriteLine("Launching ACT...");
+Process.Start(ACTPath);
+
+LauncherPath = ini.IniReadValue("Paths", "XIVLauncher");
+Console.WriteLine("Launching FFXIVLauncher...");
+Process.Start(LauncherPath);
+
+Environment.Exit(0);
